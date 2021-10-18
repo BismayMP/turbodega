5e9b4e2fe10c3e63561dee0da2d65349f9445ed6
Bismay <bismaymp@gmail.com>
2021-10-17 22:01:50 -0400

    show product list

---
+++ src/components/ProductCardComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section horizontal>
+      <div class="card-image" v-if="product?.icon" avatar>
+        <q-icon :name="product?.icon" />
+      </div>
+      <q-card-section>
+        <span style="opacity: 0.7;">{{ product?.name }}</span>
+        <br />
+        <span style="color: green; font-weight: bolder;"
+          >S/ {{ product?.price }}</span
+        >
+        x
+        {{ product?.unit }}
+        <br />
+        <span
+          ><q-icon name="info" /> Disponible {{ product?.available }}
+          {{ product?.unit }}</span
+        >
+      </q-card-section>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Product } from './models';
+
+export default defineComponent({
+  name: 'ProductCard',
+  props: {
+    product: {
+      type: Object as PropType<Product>,
+      required: true,
+    },
+  },
+  setup(props) {
+    return { ...props };
+  },
+});
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,10 @@
 <template>
   <q-page class="row page-container">
     <UserWidget :name="`Yulisa Torres`" :venta="245.0"></UserWidget>
-    <product-list />
+    <div class="product-list">
+      <h4>Mis Productos</h4>
+      <ProductList :products="list" />
+    </div>
   </q-page>
 </template>
 
@@ -10,8 +13,79 @@ import UserWidget from 'components/UserComponent.vue';
 import ProductList from 'components/ProductListComponent.vue';
 import { defineComponent } from 'vue';
 
+const productList = [
+  {
+    id: 1,
+    name: 'Galleta de soda Field paquete 6 und',
+    price: 2.3,
+    available: 4,
+    unit: 'unid',
+    icon: 'school',
+  },
+  {
+    id: 2,
+    name: 'Arroz extra Costeño bolsa 5kg',
+    price: 15.5,
+    available: 8,
+    unit: 'unid',
+    icon: 'code',
+  },
+  {
+    id: 3,
+    name: 'Galleta de soda Field paquete 6 und',
+    price: 2.3,
+    available: 4,
+    unit: 'unid',
+    icon: 'school',
+  },
+  {
+    id: 4,
+    name: 'Arroz extra Costeño bolsa 5kg',
+    price: 15.5,
+    available: 8,
+    unit: 'unid',
+    icon: 'code',
+  },
+  {
+    id: 5,
+    name: 'Galleta de soda Field paquete 6 und',
+    price: 2.3,
+    available: 4,
+    unit: 'unid',
+    icon: 'school',
+  },
+  {
+    id: 6,
+    name: 'Arroz extra Costeño bolsa 5kg',
+    price: 15.5,
+    available: 8,
+    unit: 'unid',
+    icon: 'code',
+  },
+  {
+    id: 7,
+    name: 'Galleta de soda Field paquete 6 und',
+    price: 2.3,
+    available: 4,
+    unit: 'unid',
+    icon: 'school',
+  },
+  {
+    id: 8,
+    name: 'Arroz extra Costeño bolsa 5kg',
+    price: 15.5,
+    available: 8,
+    unit: 'unid',
+    icon: 'code',
+  },
+];
+
 export default defineComponent({
   name: 'PageIndex',
   components: { UserWidget, ProductList },
+
+  setup() {
+    return { list: productList };
+  },
 });
 </script>
